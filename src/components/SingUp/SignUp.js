693eec126b5457951a26d85c46713320026cca2b
Abhishek Tiwari <65460196+Abhishek060201@users.noreply.github.com>
2023-04-05 23:20:26 +0530

    saved new backend server link (vercel)

--- src/components/SingUp/SignUp.js
+++ src/components/SingUp/SignUp.js
@@ -20,7 +20,7 @@ const SignUp = () => {
   const signupWithCreds = async (e) => {
     e.preventDefault()
     if (username !== '' && email !== '' && password !== '') {
-      const res = await axios.post('https://abhishek-chat-app-backend.herokuapp.com/signup', {
+      const res = await axios.post('https://abhishek-chat-app-backend.vercel.app/signup', {
         username,
         email,
         password
